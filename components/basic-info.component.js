e9dc595ddda628ccc5e43a796e43f3c8741aa01a
aerdukant <aerdukant@bee2code.com>
2022-01-12 19:02:15 +0100

    Added connected icon

--- components/basic-info.component.js
+++ components/basic-info.component.js
@@ -15,21 +15,26 @@ const initReloadingBasicInfoData = async () => {
 }
 
 let isConnected = true
-const reloadBasicInfoData = (operatingSystem) =>{
+const reloadBasicInfoData = async (operatingSystem) => {
     // TODO uncomment on review
-    // isConnected = await getIsInternetConnected()
+    isConnected = await getIsInternetConnected()
     const ip = getIp()
     const hostname = getHostname()
     const uptime = getUptime()
     const arch = getArch()
+    const connectionIcon = isConnected
+        ? `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#4caf50"><path d="M0 0h24v24H0V0zm0 0h24v24H0V0z" fill="none"/><path d="M1 9l2 2c4.97-4.97 13.03-4.97 18 0l2-2C16.93 2.93 7.08 2.93 1 9zm8 8l3 3 3-3c-1.65-1.66-4.34-1.66-6 0zm-4-4l2 2c2.76-2.76 7.24-2.76 10 0l2-2C15.14 9.14 8.87 9.14 5 13z"/></svg>`
+        : `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#ff1744"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21 11l2-2c-3.73-3.73-8.87-5.15-13.7-4.31l2.58 2.58c3.3-.02 6.61 1.22 9.12 3.73zm-2 2c-1.08-1.08-2.36-1.85-3.72-2.33l3.02 3.02.7-.69zM9 17l3 3 3-3c-1.65-1.66-4.34-1.66-6 0zM3.41 1.64L2 3.05 5.05 6.1C3.59 6.83 2.22 7.79 1 9l2 2c1.23-1.23 2.65-2.16 4.17-2.78l2.24 2.24C7.79 10.89 6.27 11.74 5 13l2 2c1.35-1.35 3.11-2.04 4.89-2.06l7.08 7.08 1.41-1.41L3.41 1.64z"/></svg>`
 
-    basicInfoElement.innerHTML =`
+    basicInfoElement.innerHTML = `
     <div>System: ${operatingSystem}</div/>
     <div>IP: ${ip}</div>
     <div>Hostname: ${hostname}</div>
     <div>Uptime: ${uptime}</div>
     <div>Arch: ${arch}</div>
-    <div>Connected: ${isConnected}</div>`
+    <div class="is-connected-container">
+        Connected:${connectionIcon}
+    </div>`
 }
 
 module.exports = {
